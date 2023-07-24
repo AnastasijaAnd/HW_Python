0a48c634a8f2b865b32ee1e1d10d3222bebb9870
AnastasijaAnd <rosaamina@mail.ru>
2023-07-24 20:18:24 +0300

    83

--- Telephone/database.py
+++ Telephone/database.py
@@ -1,13 +1,13 @@
 from view import *
 
 def write_name(name):
-    with open("telefon.txt", "a", encoding="UTF-8") as file:
+    with open("telphon.txt", "a", encoding="UTF-8") as file:
         file.write(name)
 
 def search_smth_in_phone_book():
     number = input_search_option()
     look_world = input_want_to_find()
-    with open("telefon.txt", "r", encoding="UTF-8") as file:
+    with open("telphon.txt", "r", encoding="UTF-8") as file:
         lst = file.readlines()
         fnd_lst = []
         if number == 1:
@@ -26,7 +26,7 @@ def search_smth_in_phone_book():
  
 
 def change_smth_in_phone_book():
-    with open("telefon.txt", "r", encoding="UTF-8") as file:
+    with open("telphon.txt", "r", encoding="UTF-8") as file:
         lst = file.readlines()
         print("Что нужно изменить?:")
         print(*search_smth_in_phone_book())
@@ -35,12 +35,12 @@ def change_smth_in_phone_book():
         for i in range(len(lst)):
             if lst[i].split(',')[0] == id:
                 lst[i] = id + "," + change_name_and_phone()    
-    with open("telefon.txt", "w", encoding="UTF-8") as file:
+    with open("telephon.txt", "w", encoding="UTF-8") as file:
         for row in lst:
             file.write(row)
 
 def delete_smth_in_phone_book():
-    with open("telefon.txt", "r", encoding="UTF-8") as file:
+    with open("telephon.txt", "r", encoding="UTF-8") as file:
         lst = file.readlines()
         print("Вы хотите удалить?:")
         print(*search_smth_in_phone_book())
@@ -48,12 +48,12 @@ def delete_smth_in_phone_book():
         for i in range(len(lst)):
             if lst[i].split(',')[0] == id:
                 lst[i] =''
-    with open("telefon.txt", "w", encoding="UTF-8") as file:
+    with open("telephon.txt", "w", encoding="UTF-8") as file:
         for row in lst:
             file.write(row)
 
 def all_phone_book():
-     with open("telefon.txt", "r", encoding="UTF-8") as file:
+     with open("telephon.txt", "r", encoding="UTF-8") as file:
         lst = file.readlines()
         for row in lst:
             print(row)
